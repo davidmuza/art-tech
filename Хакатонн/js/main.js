571871c682a1e98003134108edcbe9e435e64085
muzati <105732441+davidmuza@users.noreply.github.com>
2023-12-19 15:06:30 +0300

    Delete Хакатонн directory

--- Хакатонн/js/main.js
+++
@@ -1,6 +0,0 @@
-$(".header__list-link").on("click", function(e) {
-	e.preventDefault()
-	var id = $(this).attr('href'),
-	top = $(id).offset().top
-	$("body, html").animate({scrollTop: top}, 1000)
-})
